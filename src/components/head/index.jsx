e61fea63056e58867d4675a99ca3e6ff1e5a719b
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-25 19:55:30 -0300

    refac do head

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -5,13 +5,27 @@ import Logo from "../../../public/images/logo.png";
 export default function Cabecalho() {
   return (
     <div className={styles.head}>
-      <Image src={Logo} width={150} height={50} alt={"logo"} response="true" />
-      <input
-        type="text"
-        id="pesquisa"
-        placeholder="busque por algo"
-        name="pesquisa"
-      />
+      <div className={styles.container}>
+        <Image
+          src={Logo}
+          width={150}
+          height={50}
+          alt={"logo"}
+          response="true"
+          className={styles.logo}
+        />
+        <input
+          type="search"
+          id="pesquisa"
+          placeholder="busque por algo"
+          name="pesquisa"
+          className={styles.input}
+        />
+      </div>
+      <div className={styles.avatar}>
+        <div className={styles.foto_avatar}></div>
+        <p>johnatan</p>
+      </div>
     </div>
   );
 }
