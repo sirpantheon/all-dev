fe4585f0f8232a6a2e168bb7b68eae3652eca235
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-08-25 21:15:21 -0300

    sub-menu-bottom

--- src/components/sub-menu/index.jsx
+++ src/components/sub-menu/index.jsx
@@ -3,21 +3,37 @@ import styles from "./sub-menu.module.scss";
 export default function SubMenu() {
   return (
     <div className={styles.sub_menu}>
-      <div className={styles.sub_menu_list}>
-        <label>seu projeto</label>
-        <input type="text" placeholder="Nome do seu projeto" name="projeto" className={styles.input_projeto}/>
-        <textarea
-          name="descrição"
-          placeholder="Descrição do seu projeto"
-          rows="4"
-          cols="20"
-        />
-      <div>
-        <label>personalização</label>
-        <input type="color" name="cor" />
-      </div>
-        <input type="submit" name="enviar" placeholder="salvar" value="SALVAR PROJETO" />
-      </div>
+      <form action="" method="get" id="sub_menu">
+        <div className={styles.sub_menu_list}>
+          <label>seu projeto</label>
+          <input
+            type="text"
+            placeholder="Nome do seu projeto"
+            name="projeto"
+            className={styles.input_projeto}
+          />
+          <textarea
+            name="descrição"
+            placeholder="Descrição do seu projeto"
+            rows="4"
+            cols="20"
+          />
+          <div className={styles.sub_menu_bottom}>
+            <label>personalização</label>
+            <select name="linguagens">
+              <option value="python">Python</option>
+              <option value="javascript" selected>
+                Javascript
+              </option>
+              <option value="typescript">TypeScript</option>
+            </select>
+            <input type="color" name="cor" />
+          </div>
+        </div>
+      </form>
+      <button type="submit" form="sub_menu" value="Submit">
+        Salvar projeto
+      </button>
     </div>
   );
 }
